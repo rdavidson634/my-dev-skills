ab9e836359c7de877e1562cea020bb5ce4f5fa8f
rdavidson634 <rdavidson634@gmail.com>
2020-08-20 11:49:45 -0600

    initial commit

---
+++ models/skill.js
@@ -0,0 +1,39 @@
+let skills = [
+    {id: 1234, language: 'HTML', done: false},
+    {id: 1235, language: 'CSS', done: false},
+    {id: 1236, language: 'Javascript', done: false},
+    {id: 1237, language: 'Python', done: false}
+];
+
+module.exports = {
+    getAll,
+    getOne,
+    create,
+    deleteOne,
+    update
+};
+
+function update (id, skill) {
+    const idx = skills.findIndex(skill => skill.id === parseInt(id));
+    skill.id = parseInt(id);
+    skills.splice(idx, 1, skill)
+}
+
+function deleteOne(id) {
+    const idx = skills.findIndex(skill => skill.id === parseInt(id));
+    skills.splice(idx, 1);
+}
+
+function create (skill) {
+    skill.id = Date.now() % 1000000;
+    skill.done = false;
+    skills.push(skill);
+}
+
+function getOne(id) {
+    return skills.find(skill => skill.id === parseInt(id));
+}
+
+function getAll() {
+    return skills;
+}
